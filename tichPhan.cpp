7b4bc989911754ec4d92f30e9a954bd8d8f01ce1
khoile123 <leminhkhoi4410@gmail.com>
2021-09-29 10:53:49 +0700

    first update

--- tichPhan.cpp
+++ tichPhan.cpp
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
 
-float main() {
-	printf("Chuong trinh tinh tich phan ham f(x)=x^2\r\n");
-
-	float a, b, h, x = 0, y = 0;
-	
-	printf("Nhap a: ");
-	scanf("%f", &a); 
-	
-	printf("\r\nNhap b: ");
-	scanf("%f", &b);
-	h = (b-a) / 1000; 
+int main() {
+	printf("Chuong trinh tinh tich phan ham f(x)=x^2 trong khoang [a;b]\r\n");
+ 	float a, b, h;
 	float s = 0;
 	float dientich = 0;
-	for (float i = a; i < b; i +=h) {
-		float j = i + h;
-			x = i * i;
-			y = j * j;
-			s = ((x + y) / 2) * h;
+	
+	printf("Nhap a: ");
+		scanf("%f", &a); 	
+	printf("\r\nNhap b: ");
+		scanf("%f", &b);
+	
+	h = (b-a) / 1000; 
+	
+	for (float i = a; i < b; i +=h) 
+		{
+			float j = i + h;
+			s = ((i*i + j*j) / 2) * h;
 			dientich  += s;
-	}
-	printf("Ket qua la: %f\r\n", dientich);
+		}
+	printf("Ket qua la: %0.2f\r\n", dientich);
 	return 0;
 }
